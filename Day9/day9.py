12b8dc6da9f3e0c9b75ecf4889e83133f2c0be3b
Neil <31366098+neilneil2000@users.noreply.github.com>
2023-12-09 11:20:46 +0000

    Simplify calculating next value

--- Day9/day9.py
+++ Day9/day9.py
@@ -12,23 +12,19 @@ def main():
 def parse_input():
     return [list(map(int,row.split())) for row in day9_input.splitlines()]
 
+def extrapolate(sequence, value,backwards:bool=False)->int:
+    """Extrapolate sequence by value in direction indicated"""
+    if backwards:
+        return sequence[0]-value
+    return sequence[-1]+value
 
-def next_value(working_data:List)-> int:
+def get_next_value(working_data:List, backwards=False)-> int:
     """Get next_value in sequence"""
     for row in working_data[::-1]:
         if len(set(row))==1:
             value=row[0]
             continue
-        value = row[-1]+value
-    return value
-
-def previous_value(working_data:List)-> int:
-    """Get next_value in sequence"""
-    for row in working_data[::-1]:
-        if len(set(row))==1:
-            value=row[0]
-            continue
-        value = row[0]-value
+        value = extrapolate(row,value, backwards)
     return value
 
 def build_all_rows(sequence:List)->List:
@@ -38,16 +34,13 @@ def build_all_rows(sequence:List)->List:
         working_data.append(get_differences(working_data[-1]))
     return working_data
 
-
-
 def get_differences(sequence:List)->List:
     """Returns a list of differences between entries in input list"""
     return [sequence[i+1]-sequence[i] for i in range(len(sequence)-1)]
 
 def get_total(input_data:List, backwards:bool=False)->int:
     """Compute Total for each part. Set backwards=True for part 2"""
-    process=previous_value if backwards else next_value
-    return sum(process(build_all_rows(sequence)) for sequence in input_data)
+    return sum(get_next_value(build_all_rows(sequence),backwards) for sequence in input_data)
 
     
 
