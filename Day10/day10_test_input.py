143b5d028d2972742aa44a09c5c1638b34b8c850
Neil <31366098+neilneil2000@users.noreply.github.com>
2023-12-10 20:32:26 +0000

    As far as I got with Day 10

--- Day10/day10_test_input.py
+++ Day10/day10_test_input.py
@@ -1,8 +1,19 @@
-day10_input="""7-F7-
+day10_input_old="""7-F7-
 .FJ|7
 SJLL7
 |F--J
 LJ.LJ"""
+
+day10_input="""FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
 
 """
 7-F7-
