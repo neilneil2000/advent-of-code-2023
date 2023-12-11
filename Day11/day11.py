78e563982feddea85842b1bffb96f7bf85d24eb2
Neil Jeffery <neil.jeffery@bt.com>
2023-12-11 11:32:55 +0000

    Day 11 Part 2

--- Day11/day11.py
+++ Day11/day11.py
@@ -12,23 +12,42 @@ def main():
 def part1(universe):
     """Computer answer to part1"""
     display_universe(universe)
-    universe = expand_universe(universe)
-    display_universe(universe)
+    rows, columns = expand_universe(universe)
 
     galaxies = get_galaxy_locations(universe)
 
     distances = []
     for index, galaxy_a in enumerate(galaxies):
         for galaxy_b in galaxies[index + 1 :]:
-            distances.append(get_distance_between_galaxies(galaxy_a, galaxy_b))
+            distances.append(
+                get_distance_between_galaxies(galaxy_a, galaxy_b, rows, columns)
+            )
 
     return sum(distances)
 
 
-def get_distance_between_galaxies(galaxy_a, galaxy_b):
+def get_distance_between_galaxies(galaxy_a, galaxy_b, rows, columns):
+    """Return Distance between galaxies (Expanded Manhattan)"""
     a_x, a_y = galaxy_a
     b_x, b_y = galaxy_b
-    return abs(a_x - b_x) + abs(a_y - b_y)
+    multiplier = 1_000_000 - 1
+    return (
+        abs(a_x - b_x)
+        + abs(a_y - b_y)
+        + (get_entries_in_range(a_x, b_x, columns) * multiplier)
+        + (get_entries_in_range(a_y, b_y, rows) * multiplier)
+    )
+
+
+def get_entries_in_range(start, finish, entries):
+    """Returns number of entries between start and finish"""
+    if start > finish:
+        finish, start = start, finish
+    counter = 0
+    for entry in entries:
+        if start < entry < finish:
+            counter += 1
+    return counter
 
 
 def get_galaxy_locations(universe: List[List[str]]) -> List[Tuple[int]]:
@@ -47,7 +66,26 @@ def display_universe(universe):
     print()
 
 
-def expand_universe(universe: List) -> None:
+def get_expanded_rows(universe):
+    """Returns list of row indices that need expansion"""
+    empty_rows = []
+    for index, row in enumerate(universe):
+        if set(row) == {"."}:
+            empty_rows.append(index)
+    return empty_rows
+
+
+def get_expanded_columns(universe):
+    """Returns list of column indices that need expansion"""
+    universe_tr = list(map(list, zip(*universe)))
+    return get_expanded_rows(universe_tr)
+
+
+def expand_universe(universe: List):
+    return get_expanded_rows(universe), get_expanded_columns(universe)
+
+
+def expand_universe_old(universe: List) -> None:
     """Expand blank rows in both directions"""
     universe = expand_universe_1d(universe)
     universe = list(map(list, zip(*universe)))
