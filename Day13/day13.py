ca8ed608d2a9482bd02f445f9eca5e6c320fc67e
Neil Jeffery <neil.jeffery@bt.com>
2023-12-13 13:09:39 +0000

    Day 13 Part 2 Complete

--- Day13/day13.py
+++ Day13/day13.py
@@ -1,6 +1,31 @@
 from day13_input import day13_input
 
 
+def get_row_reflection_with_smudge(landscape):
+    """Return row of reflection"""
+    errors = 0
+    # display_landscape(landscape)
+    for index, _ in enumerate(landscape):
+        if errors == 1:
+            return index - 1
+        if index == 0:
+            continue
+        errors = 0
+        for top, bottom in zip(landscape[:index][::-1], landscape[index:]):
+            if errors > 1:
+                break
+            if top is None or bottom is None:
+                continue
+            if top != bottom:
+                for index_2, _ in enumerate(top):
+                    if top[index_2] == bottom[index_2]:
+                        continue
+                    errors += 1
+    if errors == 1:
+        return index
+    return 0
+
+
 def get_row_reflection(landscape):
     """Return row of reflection"""
     for index, _ in enumerate(landscape):
@@ -8,8 +33,6 @@ def get_row_reflection(landscape):
             continue
         for top, bottom in zip(landscape[:index][::-1], landscape[index:]):
             flag = True
-            print("".join(top))
-            print("".join(bottom))
             if top is None:
                 return index
             if bottom is None:
@@ -52,9 +75,22 @@ def part1(landscapes):
     return row_total * 100 + column_total
 
 
+def part2(landscapes):
+    row_total = 0
+    column_total = 0
+    for landscape in landscapes:
+        row = get_row_reflection_with_smudge(landscape)
+        row_total += row
+        if not row:
+            column_total += get_row_reflection_with_smudge(transpose(landscape))
+    return row_total * 100 + column_total
+
+
 def main():
     parsed = parse_input()
     result = part1(parsed)
+    print(result)
+    result = part2(parsed)
     print(result)
 
 
