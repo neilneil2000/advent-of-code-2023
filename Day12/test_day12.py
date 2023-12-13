63c48074a3d7e585881aab4cc4d91377b299657b
Neil Jeffery <neil.jeffery@bt.com>
2023-12-13 17:14:00 +0000

    Day 12 Part 2

--- Day12/test_day12.py
+++ Day12/test_day12.py
@@ -1,7 +1,64 @@
-from day12 import is_valid_combination
+from day12 import (
+    is_valid_combination,
+    good_so_far,
+    has_potential,
+    count_combinations,
+    get_all_start_points,
+)
 
 
 def test_is_valid_combination():
     assert not is_valid_combination("???.###", [1, 1, 3])
     assert not is_valid_combination("#.#.###", [1, 2, 3])
     assert is_valid_combination("#.#.###", [1, 1, 3])
+
+
+def test_good_so_far():
+    assert good_so_far("?.#.#?#", [1, 1, 3])
+    assert good_so_far("?.?.###", [1, 1, 3])
+    assert good_so_far("???.###", [1, 1, 3])
+    assert good_so_far("?.#.###", [1, 1, 3])
+    assert good_so_far("???????", [1, 1, 3])
+    assert good_so_far("?.??.##", [2, 2])
+    assert not good_so_far("....####", [1, 1, 3])
+
+
+def test_has_potential():
+    assert has_potential("?.#.#?#", [1, 1, 3])
+    assert has_potential("?.?.###", [1, 1, 3])
+    assert has_potential("???.###", [1, 1, 3])
+    assert has_potential("?.#.###", [1, 1, 3])
+    assert has_potential("???????", [1, 1, 3])
+    assert not has_potential("?.??.##", [2, 2])
+    assert not has_potential("....####", [1, 1, 3])
+    assert has_potential("???.????", [1, 4])
+    assert has_potential("?##?..??", [4, 2])
+    assert has_potential("?##?..??", [4, 1])
+    assert has_potential(".??..??...?##.", [1, 1, 3])
+    assert has_potential("?.#.#?#", [1, 3])
+
+
+def test_count_combinations():
+    assert count_combinations("?.???.?#?###?", [1, 1, 1, 3]) == (4, False)
+    assert count_combinations("#??.#.??????##?", [1, 1, 7]) == (2, False)
+    assert count_combinations("?????...#??", [5, 1]) == (1, False)
+    assert count_combinations("?.#.#?#", [1, 3]) == (2, False)
+    assert count_combinations(".??..??...?##.", [1, 1, 3]) == (4, False)
+    # assert (
+    #    count_combinations(
+    #        "?###??????????###??????????###??????????###??????????###????????",
+    #        [3, 2, 1, 3, 2, 1, 3, 2, 1, 3, 2, 1, 3, 2, 1],
+    #    )
+    #    == (506250, False)
+    # )
+
+
+def test_get_all_start_points():
+    assert get_all_start_points("?.#.#?#", 1) == [0, 2, 4, 6]
+    assert get_all_start_points("?.#.#?#", 2) == []
+    assert get_all_start_points("?.#.#?#", 3) == [4]
+    assert get_all_start_points("?.#.#?#", 4) == []
+    assert get_all_start_points("?.#.#?#", 5) == []
+    assert get_all_start_points("?.#.#?#", 6) == []
+    assert get_all_start_points("?#?#?#?", 2) == [0, 5]
+    assert get_all_start_points("..#??", 1) == [2]
