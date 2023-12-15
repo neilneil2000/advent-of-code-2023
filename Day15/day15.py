174aa8eba01e2bf7f50d8f18ff7a16df23a2ab89
Neil Jeffery <neil.jeffery@bt.com>
2023-12-15 08:47:16 +0000

    Day 15 Part 1 Tidy Up

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,17 +1,21 @@
+"""Advent of Code 2023 Day 15"""
 from day15_input import day15_input
 
 
 def main():
-    instructions = [s for s in day15_input.split(",")]
+    """Main Function"""
+    instructions = day15_input.split(",")
     part1_result = part1(instructions)
     print(part1_result)
 
 
 def part1(instructions):
-    return sum(hash(c) for c in instructions)
+    """Find solution to part 1"""
+    return sum(hasher(c) for c in instructions)
 
 
-def hash(instruction: str) -> int:
+def hasher(instruction: str) -> int:
+    """Compute hash on string"""
     current_value = 0
     for character in instruction:
         current_value += ord(character)
