fff81b5b2eb6a78ed46e46c0b14b22846fc3ef06
Neil Jeffery <neil.jeffery@bt.com>
2023-12-15 09:16:26 +0000

    Day 15 Part 2 Complete

--- Day15/day15.py
+++ Day15/day15.py
@@ -8,11 +8,26 @@ def main():
     instructions = day15_input.split(",")
     part1_result = part1(instructions)
     print(part1_result)
+    part2_result = part2(instructions)
+    print(part2_result)
 
 
 def part1(instructions):
     """Find solution to part 1"""
     return sum(Hashmap.hasher(c) for c in instructions)
+
+
+def part2(instructions):
+    """Find solution to part 2"""
+    hashmap = Hashmap()
+    for i in instructions:
+        if "=" in i:
+            label, focal = i.split("=")
+            hashmap.set(label, int(focal))
+            continue
+        label = i[:-1]
+        hashmap.remove(label)
+    return hashmap.score
 
 
 if __name__ == "__main__":
--- Day15/hashmap.py
+++ Day15/hashmap.py
@@ -1,12 +1,35 @@
 class Hashmap:
-    def _init_(self):
-        self._hashmap = {i: [] for i in range(256)}
+    """Represents Hashmap"""
 
-    def add(self, value, location):
-        self._hashmap[hash(location)].append((location, value))
+    def __init__(self):
+        self._hashmap = [[] for _ in range(256)]
 
-    def remove(self, value, location):
-        self._hashmap[hash(location)].remove((location, value))
+    def set(self, label, value):
+        """Add value at location"""
+        location = self.hasher(label)
+        for slot, stored in enumerate(self._hashmap[location]):
+            stored_label = stored[0]
+            if label == stored_label:
+                self._hashmap[location][slot] = (label, value)
+                return
+        self._hashmap[location].append((label, value))
+
+    def remove(self, label):
+        """remove value from location"""
+        location = self.hasher(label)
+        for stored_label, value in self._hashmap[location]:
+            if label == stored_label:
+                self._hashmap[location].remove((label, value))
+
+    @property
+    def score(self):
+        """Compute score"""
+        total = 0
+        for number, contents in enumerate(self._hashmap):
+            for slot, stored in enumerate(contents):
+                focal_length = stored[1]
+                total += (number + 1) * (slot + 1) * focal_length
+        return total
 
     @classmethod
     def hasher(cls, value) -> int:
