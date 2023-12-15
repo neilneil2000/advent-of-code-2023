058282ff36920db34cd35eb78a9a462db993fb67
Neil Jeffery <neil.jeffery@bt.com>
2023-12-15 12:55:34 +0000

    Tidy Day 3 Output

--- Day3/day3.py
+++ Day3/day3.py
@@ -3,16 +3,18 @@ from typing import List
 from schematic import Schematic
 from day3_input import day3_input
 
+
 def main():
     schematic = parse_input()
     gondola_engine_diagram = Schematic(schematic)
-    print(gondola_engine_diagram.part_number_total())
-    print(gondola_engine_diagram.gear_ratio_total())
+    print(f"Part1: {gondola_engine_diagram.part_number_total()}")
+    print(f"Part2: {gondola_engine_diagram.gear_ratio_total()}")
 
-def parse_input()->List:
+
+def parse_input() -> List:
     """Return structured version of input file"""
     return [list(line) for line in day3_input.splitlines()]
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
