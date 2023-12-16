d32ff6b2c762e48d66b1b615d209846cc1b75b43
Neil <31366098+neilneil2000@users.noreply.github.com>
2023-12-16 17:47:23 +0000

    Day 17 Prep

---
+++ Day17/day17.py
@@ -0,0 +1,31 @@
+"""Advent of Code 2023 Day 17"""
+
+from day17_test_input import day17_input
+
+
+def main():
+    """Main Solution"""
+    data = parse_input()
+    part1_answer = part1(data)
+    print(f"Part 1: {part1_answer}")
+
+    part2_answer = part2(data)
+    print(f"Part 2: {part2_answer}")
+
+
+def part1(data):
+    """Solution to Part 1"""
+    return 0
+
+
+def part2(data):
+    """Solution to Part 2"""
+    return 0
+
+
+def parse_input():
+    return day17_input
+
+
+if __name__ == "__main__":
+    main()
