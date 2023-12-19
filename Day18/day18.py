65e00a93320f25b178aecffddfdced959ca5b968
Neil Jeffery <neil.jeffery@bt.com>
2023-12-19 08:13:54 +0000

    Day 18 So Far

--- Day18/day18.py
+++ Day18/day18.py
@@ -1,4 +1,4 @@
-from day18_input import day18_input
+from day18_test_input import day18_input
 from lagoon import Lagoon
 
 
@@ -7,15 +7,27 @@ def main():
     lava_lagoon = Lagoon(data)
     lava_lagoon.dig_perimeter()
     lava_lagoon.dig_internal()
+    # lava_lagoon.display()
+    # lava_lagoon.flood_fill_outside()
+    # lava_lagoon.display()
+    # print(f"Part 1: {lava_lagoon.volume}")
+
+    # lava_lagoon.convert_hex_instructions()
+    # lava_lagoon.dig_perimeter()
     lava_lagoon.display()
-    lava_lagoon.flood_fill_outside()
-    lava_lagoon.display()
-    print(lava_lagoon.volume)
+    lava_lagoon.build_vertices()
+    lava_lagoon.compute_volume()
+    total = 0
+    while len(lava_lagoon.path) > 2:
+        total += lava_lagoon.volume_of_next_chunk()
+        lava_lagoon.display()
+
+    pass
 
 
 def parse_input():
     parsed = [tuple(row.split(" ")) for row in day18_input.splitlines()]
-    return [(row[0], int(row[1]), int(row[2].strip("()#"), 16)) for row in parsed]
+    return [(row[0], int(row[1]), row[2].strip("()#")) for row in parsed]
 
 
 if __name__ == "__main__":
--- Day18/lagoon.py
+++ Day18/lagoon.py
@@ -5,6 +5,7 @@ class Lagoon:
         self.instructions = instructions
         self.path = [(0, 0)]
         self.lagoon = None
+        self.vertices = None
 
     @property
     def digger(self):
@@ -12,6 +13,7 @@ class Lagoon:
 
     def dig_perimeter(self):
         """Dig Perimeter"""
+        self.path = [(0, 0)]
         for direction, count, _ in self.instructions:
             digger_x, digger_y = self.digger
             if direction == "R":
@@ -22,6 +24,66 @@ class Lagoon:
                 self.path.append((digger_x, digger_y - count))
             if direction == "D":
                 self.path.append((digger_x, digger_y + count))
+
+    def convert_hex_instructions(self):
+        new_instructions = []
+        directions = {"0": "R", "1": "D", "2": "L", "3": "U"}
+        for _, _, hex_code in self.instructions:
+            distance = int(hex_code[:5], 16)
+            direction = directions[hex_code[-1]]
+            new_instructions.append((direction, distance, ""))
+        self.instructions = new_instructions
+
+    def build_vertices(self):
+        self.vertices = {}
+        for x, y in self.path:
+            if y not in self.vertices:
+                self.vertices[y] = set()
+            self.vertices[y].add(x)
+
+    def compute_volume(self):
+        total = 0
+        calc_rows = []
+        for row, vertices in sorted(self.vertices.items()):
+            collapsed = []
+            for calc_row, start, end in calc_rows.copy():
+                if start not in vertices and end not in vertices:
+                    continue
+                collapsed.append([start, end])
+                calc_rows.remove((calc_row, start, end))
+                row_length = end - start + 1
+                row_depth = row - calc_row
+                total += row_length * row_depth
+            for index, vertex in enumerate(sorted(vertices)):
+                if collapsed:
+                    start, end = collapsed.pop(0)
+                    if vertex < start < end:
+                        # add e to current row and remove s from row
+                        vertices.add(end)
+                        vertices.remove(start)
+                    elif start < vertex < end:
+                        # add s to row and remove e from row
+                        vertices.add(start)
+                        vertices.remove(end)
+                        # increase total by e-vertex
+                        total += end - vertex
+                    elif start == vertex:
+                        total += sorted(vertices)[index + 1] - vertex
+                        # remove vertex and add e
+                        vertices.add(end)
+                        vertices.remove(vertex)
+                    else:
+                        # add s to row and remove vertex from row
+                        vertices.add(start)
+                        vertices.remove(vertex)
+            vertex_pairs = [
+                sorted(vertices)[i : i + 2] for i in range(0, len(vertices), 2)
+            ]
+            for a, b in vertex_pairs:
+                calc_rows.append((row, a, b))
+            pass
+
+        return total
 
     def dig_internal(self):
         x_min = sorted(self.path)[0][0]
@@ -47,6 +109,51 @@ class Lagoon:
                         self.lagoon[y - y_min + 1][intermediate - x_min + 1] = "#"
             self.lagoon[y - y_min + 1][x - x_min + 1] = "#"
             last_x, last_y = x, y
+
+    def volume_of_next_chunk(self):
+        b, c = self.get_start_line()
+        if c == len(self.path) - 1:
+            c = 0
+        a = self.path[b - 1]
+        d = self.path[(c + 1)]
+
+        total = 0
+        if a[0] > d[0]:
+            total += (abs(self.path[c][1] - d[1]) + 1) * (abs(a[0] - self.path[b][0]))
+            self.path[b] = (d[0], self.path[b][1])
+            if c == 0:
+                self.path[len(self.path) - 1] = (d[0], self.path[c][1])
+            self.path.pop(c)
+            self.path.pop(c)
+        else:
+            total += (abs(a[1] - self.path[b][1]) + 1) * (abs(a[0] - self.path[b][0]))
+            self.path[c] = (a[0], self.path[c][1])
+            if c == 0:
+                self.path[len(self.path) - 1] = (a[0], self.path[c][1])
+            self.path.pop(b)
+            self.path.pop(b - 1)
+        self.dig_internal()
+        return total
+
+    def get_start_line(self):
+        x_min = sorted(self.path)[0][0]
+        # find something that touches x_min
+        anchor = None
+        end_point = None
+        for index, location in enumerate(self.path):
+            x, _ = location
+            if x != x_min:
+                anchor = None
+                continue
+            if anchor is None:
+                anchor = index
+                continue
+            if x != self.path[anchor][0]:  # movement in x
+                anchor = None
+                continue
+            end_point = index
+            break
+        return anchor, end_point
 
     def display(self):
         for row in self.lagoon:
