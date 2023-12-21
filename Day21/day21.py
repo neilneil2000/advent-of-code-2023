8fd144908ee7e9700f1bf7a3f004eac8faab4ff1
Neil Jeffery <neil.jeffery@bt.com>
2023-12-21 09:39:40 +0000

    Day21 Part 2 Attempt 1

--- Day21/day21.py
+++ Day21/day21.py
@@ -1,10 +1,10 @@
-from day21_input import day21_input
+from day21_test_input import day21_input
 from garden import Garden
 
 
 def main():
     garden = Garden(parse_input())
-    part1_result = garden.locations_after_steps(64)
+    part1_result = garden.locations_after_steps(500, True)
     print(f"Part 1: {part1_result}")
 
 
--- Day21/garden.py
+++ Day21/garden.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import Set
 
 
@@ -23,24 +24,36 @@ class Garden:
     def length(self):
         return len(self.layout)
 
-    def locations_after_steps(self, number_of_steps):
+    def locations_after_steps(self, number_of_steps, wrap=False):
         """Returns the number of locations possible to be in after number_of_steps"""
         locations = {self.get_start()}
-        for _ in range(number_of_steps):
-            locations = self.locations_after_one_step(locations)
+        for step in range(number_of_steps):
+            locations = self.locations_after_one_step(locations, wrap)
+            print(f"Step {step+1}: {len(locations)}")
         return len(locations)
 
-    def locations_after_one_step(self, locations):
+    def locations_after_one_step(self, locations, wrap=False):
         """Returns new set of locations after one step"""
         new_locations = set()
         for location in locations:
             new_locations.update(self.adjacent_squares(location))
+        if wrap:
+            return {
+                location for location in new_locations if not self.is_rock(location)
+            }
         return {location for location in new_locations if self.is_valid(location)}
 
+    @cache
     def adjacent_squares(self, location) -> Set:
         """Return set of"""
         x, y = location
         return {(x, y + 1), (x, y - 1), (x - 1, y), (x + 1, y)}
+
+    @cache
+    def wrap_to_bounds(self, location):
+        """Returns location when forced to be within bounds"""
+        x, y = location
+        return (x % self.width, y % self.length)
 
     def is_valid(self, location):
         """Returns true if location is valid"""
@@ -48,7 +61,7 @@ class Garden:
 
     def is_rock(self, location):
         """Return True if location contains a rock"""
-        x, y = location
+        x, y = self.wrap_to_bounds(location)
         return self.layout[y][x] == "#"
 
     def is_within_bounds(self, location):
